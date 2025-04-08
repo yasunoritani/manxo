5775ce8d8e0d894ef738d2874aad2d0ef13717ff
yasunoritani <yasunoritani@gmail.com>
2025-04-09 03:06:05 +0900

    整理: 現在の実装に必要のないファイルを削除

--- libwebsockets/build_x86_64/lib/system/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mymac/v8ui/libwebsockets/source/lib/system
-# Build directory: /Users/mymac/v8ui/libwebsockets/build_x86_64/lib/system
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("smd")
-subdirs("metrics")
