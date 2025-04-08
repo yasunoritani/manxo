9d31f421ceedf31c4da0617d8016a7547908de33
yasunoritani <yasunoritani@gmail.com>
2025-04-09 02:54:15 +0900

    不要なディレクトリとファイルを削除：MinDevKit、libwebsockets、oscpackなどの依存関係を整理

--- libwebsockets/build_x86_64/lib/roles/raw-file/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mymac/v8ui/libwebsockets/source/lib/roles/raw-file
-# Build directory: /Users/mymac/v8ui/libwebsockets/build_x86_64/lib/roles/raw-file
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
