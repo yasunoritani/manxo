4770db69839c1e6d6d6cb9540f3fecbca29efe3d
yasunoritani <yasunoritani@gmail.com>
2025-04-09 19:51:06 +0900

    不要なディレクトリとファイルを削除

--- archives/libwebsockets/build_x86_64/lib/core-net/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mymac/v8ui/libwebsockets/source/lib/core-net
-# Build directory: /Users/mymac/v8ui/libwebsockets/build_x86_64/lib/core-net
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- archives/libwebsockets/build_x86_64/lib/roles/h1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mymac/v8ui/libwebsockets/source/lib/roles/h1
-# Build directory: /Users/mymac/v8ui/libwebsockets/build_x86_64/lib/roles/h1
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
